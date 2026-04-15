b4aa3daa457fffc24e6b32a8f299555cc13e1534
masraku <masendra0303@gmail.com>
2026-04-16 00:08:36 +0700

    add new project: Landing Page Mulia Berkat Abadi with description and assets

--- data/projects.js
+++ data/projects.js
@@ -17,7 +17,7 @@ export const projects = [
     liveUrl: 'https://web-pengaduan.vercel.app',
     image: '/assets/pengaduan.png',
   },
-    {
+  {
     title: 'Japanese Learning RakuGo',
     description:
       'An interactive Japanese language learning platform designed to help users master Hiragana, Katakana, and Kanji through gamified features, real-time progress tracking, streaks, quizzes, and JLPT-aligned character libraries (N5-N3).',
@@ -25,5 +25,14 @@ export const projects = [
     githubUrl: 'https://github.com/masraku/japaneselearn',
     liveUrl: 'https://rakugo.studio',
     image: '/assets/rakugo.png',
+  },
+  {
+    title: 'Landing Page Mulia Berkat Abadi',
+    description:
+      'A visually appealing landing page for Mulia Berkat Abadi, a company specializing in distributing health products. The landing page highlights the company\'s commitment to sustainability, product quality, and customer satisfaction.',
+    tech: ['Next.js', 'Tailwind', 'Framer Motion'],
+    githubUrl: 'https://github.com/masraku/gunamaju',
+    liveUrl: 'https://muliaberkatalkes.com',
+    image: '/assets/muliaberkat.png',
   },
 ];
